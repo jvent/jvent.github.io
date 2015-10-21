d3a530a9289fa42257148bc6a256d205ad85575d
JVent <jventy@outlook.com>
2015-10-21 15:24:23 -0500

    Start project from scratch
    Rewriting project to change design and content

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-var menuHamburgerButton = document.getElementById("menu-btn");
-var mainNavigationMenu = document.getElementById("main-menu");
-
-function showNavigationMenu () {
-	mainNavigationMenu.classList.toggle("hide");
-	menuHamburgerButton.classList.toggle("hamburger-icon");
-	menuHamburgerButton.classList.toggle("close-icon");
-}
